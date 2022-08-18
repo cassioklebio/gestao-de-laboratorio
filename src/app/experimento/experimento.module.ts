23a791bc039813651978e925fec1ddf20cd6dd34
cassio klebio pereira lins <cassiotr2009@gmail.com>
2022-08-18 18:00:41 -0300

    Atividade-08 - Criação do Layout segundo o prototipo além da utilização de dois data-binding

---
+++ src/app/experimento/experimento.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { ListagemComponent } from './listagem/listagem.component';
+import { EditComponent } from './edit/edit.component';
+
+
+
+@NgModule({
+  declarations: [
+    CadastroComponent,
+    ListagemComponent,
+    EditComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ExperimentoModule { }
